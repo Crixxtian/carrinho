dcc9bd2e55b51a60a7dafd39e0ddd82f60f8f7ac
Crixxtian <cristiandmachado123@gmail.com>
2023-08-16 15:40:54 -0300

    modificando o aplicativo

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
 import topo from '../../../assets/topo.png';
 
 
+const width = Dimensions.get('screen').width;
+
 export default function Carrinho(){
-    return <Image source={topo}></Image>
+    return <>
+    <Image source={topo} style={estilos.topo}></Image>
+    <Text style={estilos.titulo}>Detalhe do carrinho</Text>
+    <View>
+        <Text>Carrinho de compra</Text>
+        <Text>Fazenda Etec Cidade do Livro</Text>
+        <Text>Uma cesta com produtos selecionados cuidadosamente
+            da fazenda direto para sua cozinha 
+        </Text>
+        <Text>R$ 40,00</Text>
+    </View>
+    </>
 }
+
+const estilos= StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578/768 * width
+    },
+    titulo: {
+        fontSize: 24, 
+        fontWeight: "bold",
+        lineHeight: 26,
+        width: "100%",
+        textAlign: "center",
+        color: "white",
+        position: "absolute",
+        padding: 16
+    }
+})
