f4c9dd7546af0bf24b00a1ec1bf1709964f8794a
Crixxtian <cristiandmachado123@gmail.com>
2023-09-22 11:56:50 -0300

    componets

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,80 +1,28 @@
 import React from 'react';
-import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
-import topo from '../../../assets/topo.png';
-import logo from '../../../assets/logo.png';
+import { StyleSheet, View} from 'react-native';
 
 
-const width = Dimensions.get('screen').width;
+import Topo from './components/Topo';
+import Detalhe from './components/Detalhe';
+
+
+
 
 export default function Carrinho(){
     return <>
-    <Image source={topo} style={estilos.topo}></Image>
-
-    <Text style={estilos.titulo}>Detalhe do carrinho</Text>
-
+    
+    <Topo/>
     <View style={estilos.carrinho}>
-
-        <Text style={estilos.titulocarrinho}>Carrinho de compra</Text>
-        <View style={estilos.fazenda}>
-            <Image style={estilos.imgFazenda} source={logo}></Image>
-            <Text style={estilos.nomeFazenda}>Fazenda Etec Cidade do Livro</Text>
-        </View>
-        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente
-            da fazenda direto para sua cozinha 
-        </Text>
-        <Text style={estilos.preco}>R$ 40,00</Text>
+        <Detalhe/>
     </View>
     </>
 }
 
 const estilos= StyleSheet.create({
-    topo: {
-        width: "100%",
-        height: 578/768 * width
-    },
-    titulo: {
-        fontSize: 24, 
-        fontWeight: "bold",
-        lineHeight: 26,
-        width: "100%",
-        textAlign: "center",
-        color: "white",
-        position: "absolute",
-        padding: 16
-    },
-    titulocarrinho:{
-        fontSize: 26,
-        lineHeight: 46,
-        color: "#464646",
-        fontFamily: "MontserratBold"
-    },
+ 
     carrinho:{
         paddingVertical:8,
         paddingHorizontal: 16
-    },
-    nomeFazenda:{
-        fontSize: 16,
-        lineHeight:26,
-        marginLeft: 12,
-        fontWeight: "bold"
-    },
-    imgFazenda:{
-        width: 32,
-        height: 32
-    },
-    fazenda:{
-        flexDirection: "row",
-        paddingVertical: 12
-    },
-    descricao:{
-        color:"#a3a3a3",
-        fontStyle: "italic"
-    },
-    preco:{
-        fontSize: 26,
-        lineHeight:42,
-        color: "#2a9f85",
-        marginTop: 8
     }
 
 })
